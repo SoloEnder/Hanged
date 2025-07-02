5b0621b101ce5cbedd001a9bf7821483682fae76
SoloEnder <hounhbedjileonide@protonmail.com>
2025-07-02 18:52:06 +0200

    Start the gui version using tkinter

--- main.py
+++ main.py
@@ -1,50 +1,42 @@
 try:
+    
     import getpass
+    import tkinter as tk
     
 except ImportError:
-    print("Ce programme a besoin que le module 'getpass' soit installé")
+    print("Ce programme a besoin que les modules 'getpass' et 'tkinter' soient installés")
     exit()
     
-from program.game_styles.multi import multi_player
 
 print("Welcome to Hanged Game")
+from program.game_styles.multi import multi_player
+
 class Game:
     
-    def __init__(self):
-        self.multi = multi_player
+    def multi(self):
+        multi_player(self)
     
-    def new_game(self):
-        mod_loop = True
+    def __init__(self):
+        self.w = tk.Tk()
         
-        while mod_loop == True:
-            
-            mod_choice = input("\n=== Mod ===\n1) Multi-player\n2) Back\n")
-            
-            if mod_choice == "1":
-                self.multi()
-                
-            elif mod_choice == "2":
-                mod_loop = False
-                
-            else: 
-                print("\nInvalid choice")
+        # La frame pour la saisie de carractères et l'affichage des erreurs
+        self.entry_errors_fr = tk.Frame(self.w)
+        self.entry_errors_fr.grid(row=0, column=0)
+        self.entry_lbl = tk.Label(self.entry_errors_fr, text="Entrez le mot que les autres joueurs devront deviner")
+        self.entry_lbl.grid(row=0, pady=20, padx=10)
+        self.entry_e = tk.Entry(self.entry_errors_fr, show="*")
+        self.entry_e.grid(row=1)
+        self.alph_error = tk.Label(self.entry_errors_fr)
+        self.alph_error.grid(row=2)
+        self.enter_b = tk.Button(self.entry_errors_fr, text="Next", command=self.multi)
+        self.enter_b.grid(row=4, pady=10)
+        self.error_lbl = tk.Label(self.entry_errors_fr, fg="red")
+        self.error_lbl.grid(row=3)
         
-    def menu(self):
-        menu_loop = True
-        
-        while menu_loop == True:
-            menu_choice = input("\n===== Menu =====\n1) New Game\n2) Quit\n")
-            
-            if menu_choice == "1":
-                self.new_game()
-                
-            elif menu_choice == "2":
-                print("\nGoodbye !")
-                exit()
-            
-            else:
-                print("\nInvalid choice")
-                
-                
-game = Game()
-game.menu()
+        # Partie où sera dessiné le pendu
+        self.pendu_fr = tk.Frame(self.w)
+        self.pendu_fr.grid(row=1, column=0)
+        self.pendu_cv = tk.Canvas(self.pendu_fr, width=200, height=200, bg="white")
+        self.pendu_cv.grid(row=0, column=0, pady=20)
+        self.w.mainloop()
+Game()
--- program/game_styles/multi.py
+++ program/game_styles/multi.py
@@ -3,5 +3,5 @@ import getpass
 from program.game_styles.base import base
 
 @base
-def multi_player():
-     return getpass.getpass("\nEntrez le mot que les autres joueurs devront deviner ou cliquez sur la touche 'Enter' pour quitter : ")  
+def multi_player(self):
+     return self.entry_e.get()
